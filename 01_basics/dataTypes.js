ab7856306c0cd224d2cf685dfb606b039516dd22
Rohit-driod <sahilnayak221@gmail.com>
2024-12-31 15:19:17 +0000

    datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"
+/*
+number => 2 to the power of 53
+bigInt
+string => ""
+boolean => true/false
+null => standalone value
+undefined => value not assingned
+symbol => unique features
+
+object
+ */
+console.log(typeof null);
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,6 @@
-console.log("Rohit")
+const accountId = 1445566
+let accountEmail = "rohit@gmail.com"
+var accountPass = "12345"
+console.table([accountId, accountEmail,accountPass])
+
+
