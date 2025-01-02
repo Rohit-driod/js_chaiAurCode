d242cc27abc9b7905e090d8ea48075639ec06010
Rohit-driod <sahilnayak221@gmail.com>
2025-01-02 14:50:31 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "33aa"
+
+console.log(typeof score)
+
+let valueNumber = Number(score)
+
+console.log(typeof valueNumber)
+console.log(valueNumber)//will print NaN
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof isLoggedIn)
+console.log(isLoggedIn)
+
+console.log(typeof booleanIsLoggedIn)
+console.log(booleanIsLoggedIn)
+
+let someNumber  = 34
+let stringSomeNumber = String(someNumber)
+console.log(typeof stringSomeNumber)
+console.log(stringSomeNumber)
