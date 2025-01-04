12a41aae4096090dfdb3896d83b38bfad1f0087d
Rohit-driod <sahilnayak221@gmail.com>
2025-01-04 15:26:50 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,9 @@
+const name = 'rohit'
+const repoCount = 12
+
+//String interpolation
+console.log(`hello my name is ${name.toUpperCase()} and my repo count is ${repoCount}`);
+
+const gameName = new String('portal')
+
+console.log(gameName.length)
