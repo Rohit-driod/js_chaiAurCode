8a3c247eee1031e48433c175ed8e11b70a5f0bfc
Rohit-driod <sahilnayak221@gmail.com>
2025-01-05 13:56:20 +0000

    date

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,24 @@
+// let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+
+// let date = new Date(2023,0,23,5,3)
+// let date2 = new Date(2023,0,23,5,3)
+// console.log(date.toLocaleString())
+
+// let timeStamp = Date.now()
+// console.log(timeStamp);
+// console.log(date.getTime());
+// console.log(Math.floor(timeStamp/100));
+let newDate = new Date()
+console.log(newDate);
+
+newDate.toLocaleString('default',{
+    weekday :  "long", 
+})
+
+
+
