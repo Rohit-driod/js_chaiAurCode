50ada849e08f45cfeb6e933264506a07432ae16d
Rohit-driod <sahilnayak221@gmail.com>
2025-01-05 07:38:16 +0000

    numbers_maths

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -4,6 +4,6 @@ const repoCount = 12
 //String interpolation
 console.log(`hello my name is ${name.toUpperCase()} and my repo count is ${repoCount}`);
 
-const gameName = new String('portal')
+const gameName = new String('portal is a good game')
 
-console.log(gameName.length)
+console.log(gameName.endsWith('games'))
---
+++ 01_basics/06_numbers_maths.js
@@ -0,0 +1,24 @@
+//Numbers
+// const balance = new Number(200.74556)
+// console.log(balance.valueOf());
+
+// console.log(balance.toString)
+// console.log(balance.toFixed(2))
+// console.log(balance.toPrecision(3))
+
+// const hundreds = 100000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//Maths
+
+console.log(Math)
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log((Math.random()*10)+1);
+
+const min =100 
+const max = 200
+
+console.log(Math.floor(Math.random()*(max - min +1))+min)//important -- to generate random num bet nums..
+
+
