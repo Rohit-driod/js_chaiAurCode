7e4ebb33bca87b2aa78f70a9e585071c4663e164
Rohit-driod <sahilnayak221@gmail.com>
2025-01-11 16:24:27 +0000

    arrow

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -14,8 +14,33 @@ const user = {
 //  user.welcomeMessage()
 // console.log(this);
 
-function chai (){
-    console.log(this);
+// function chai (){
+//     let userName = 'guddu'
+//     console.log(this.userName);//'this' does not work in a function
     
-}
-chai()
+// }
+// chai()
+
+// const chai=()=>{
+//     let userName = 'guddu'
+//     console.log(this.userName);
+    
+// }
+// chai()
+
+
+// const addTwo = (num1,num2)=>{
+//     return num1+num2
+// }
+
+
+const addTwo = (num1,num2)=> num1+num2 //no need to 'return' if curly braces r not used 
+
+console.log(addTwo(1,2));
+
+const add = () =>({userName :'rohit' })//use () to return object
+console.log(add());
+
+
+
+
