816ac2acd07e3379381908219698da8c47077105
Rohit-driod <sahilnayak221@gmail.com>
2025-01-11 08:01:21 +0000

    function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -16,5 +16,36 @@ function logInUserMessage(username = 'guddu'){//default value
     return `${username} just logged in`
 }
 const username = logInUserMessage('Rohit')
-console.log(username);
+// console.log(username);
+
+function CartPrice(val1,val2,...num1){   //rest operator//important
+    return num1
+}
+
+
+// console.log(CartPrice(200,40,500,12,85,303));
+
+const user = {
+    username : 'rohit',
+    price : 45
+}
+function handleObject(anyObject){
+    console.log(`Usernameis ${anyObject['username']} and price is ${anyObject['price']}`);
+    
+
+}
+
+handleObject(user)
+handleObject({
+    username : 'guddu',
+    price : 99
+})
+
+const myArray = [200,300,600,100]
+
+function returnArray(getArray){
+    return getArray[1]
+}
+// console.log(returnArray(myArray));
+// console.log(returnArray([34,67,55,99]));
 
