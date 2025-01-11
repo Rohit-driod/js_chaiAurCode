038970f037d66cd1b4b0e9637dbe1bdfe09af5d9
Rohit-driod <sahilnayak221@gmail.com>
2025-01-11 13:09:02 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,25 @@
+function one (){
+    const userName = 'Rohit'
+
+    function two(){
+        const website= 'youtube'
+        // console.log(userName);
+        
+    }
+    // console.log(website)
+    two()
+}
+
+one()
+
+//important concept
+console.log(add1(5))
+function add1(num){
+    return num+1
+}
+
+console.log(add2(5));
+
+const add2 = function(num){
+    return num+1
+}
