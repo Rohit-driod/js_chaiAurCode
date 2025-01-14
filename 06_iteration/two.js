7e123943092fbaaeb6253a73a451fba6d00c512d
Rohit-driod <sahilnayak221@gmail.com>
2025-01-14 07:20:15 +0000

    while

---
+++ 06_iteration/two.js
@@ -0,0 +1,21 @@
+let index = 0
+
+while (index <= 10) {
+    // console.log(`value of index ${index}`);
+    index = index + 2
+    
+}
+let myArray = ['flash', "batman", "superman"]
+
+let num = 0
+while(num < myArray.length){
+    // console.log(`value is ${myArray[num]}`);
+    num++
+}
+
+
+let score = 11
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
