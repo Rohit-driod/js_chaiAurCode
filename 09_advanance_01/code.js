b2bd24387221c021fd1e223b80a643fa5c5a64d3
Rohit Raj Tirkey <sahilnayak221@gmail.com>
2025-01-28 14:09:11 +0000

    api

--- 09_advanance_01/code.js
+++ 09_advanance_01/code.js
@@ -1,12 +1,41 @@
+ 
+const button = document.querySelector('button')
 
-requestUrl='https://api.qithub.com/users/hiteshchoudhary'
-const xhr = new XMLHttpRequest()
-xhr.open( 'GET',requestUrl)
-console.log(xhr.readyState);
-xhr.onreadystatechange = function(){
+button.addEventListener('click',()=>{
     console.log(xhr.readyState);
+    xhr.onreadystatechange = function(){
+    console.log(xhr.readyState);
+    if(xhr.readyState === 4){
+        const data = JSON.parse(this.responseText)
+        console.log( data);
+        
+        console.log(data.avatar_url);
+        document.querySelector('.idCard').style.backgroundImage = `url(${data.avatar_url})`
+        
+    }
     
 }
 
 xhr.send();
+})
+
+
+requestUrl='https://api.github.com/users/hiteshchoudhary'
+const xhr = new XMLHttpRequest()
+xhr.open( 'GET',requestUrl)
+console.log(xhr.readyState);
+// xhr.onreadystatechange = function(){
+//     console.log(xhr.readyState);
+//     if(xhr.readyState === 4){
+//         const data = JSON.parse(this.responseText)
+//         console.log( data);
+        
+//         console.log(data.avatar_url);
+        
+//     }
+    
+// }
+
+// xhr.send();
+
 
